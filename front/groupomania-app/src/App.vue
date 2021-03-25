cf887bc384da8f75332849aef087be6d2fcf3bf2
gaellegaret <gaelle.garet@gmail.com>
2021-03-25 12:10:10 +0100

    add Alert vue

--- front/groupomania-app/src/App.vue
+++ front/groupomania-app/src/App.vue
@@ -1,41 +1,18 @@
 <template>
   <div id="app">
     <Nav />
-    <div v-show="alert.message" class="modal is-active">
-      <div class="modal-background"></div>
-      <div class="modal-content">
-        <div :class="alert.type" class="message">
-          <div class="message-header">
-            <p v-if="alert.type=='is-danger'">Erreur</p>
-            <p v-if="alert.type=='is-success'">Succès</p>
-            <button class="delete" aria-label="delete" @click="clearAlert"></button>
-          </div>
-          <div class="message-body">
-            {{alert.message}}
-          </div>
-        </div>
-      </div>
-    </div>
+    <Alert />
     <router-view/>
   </div>
 </template>
 <script>
 import Nav from './components/partials/Nav.vue';
-import { mapState, mapActions } from 'vuex'
+import Alert from './components/partials/Alert.vue';
 export default {
   name: 'app',
   components: {
-    Nav
-  },
-  computed: {
-        ...mapState({
-            alert: state => state.alert
-        })
-    },
-  methods: {
-      ...mapActions({
-          clearAlert: 'alert/clear' 
-      })
+    Nav,
+    Alert
   }
 }
 </script>
---
+++ front/groupomania-app/src/components/partials/Alert.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-show="alert.message" class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+        <div :class="alert.type" class="message">
+            <div class="message-header">
+            <p v-if="alert.type=='is-danger'">Erreur</p>
+            <p v-if="alert.type=='is-success'">Succès</p>
+            <button class="delete" aria-label="delete" @click="clearAlert"></button>
+            </div>
+            <div class="message-body">
+            {{alert.message}}
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  name: 'Alert',
+  computed: {
+        ...mapState({
+            alert: state => state.alert
+        })
+    },
+  methods: {
+      ...mapActions({
+          clearAlert: 'alert/clear' 
+      })
+  }
+}
+</script>
